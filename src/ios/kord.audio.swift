40a32ca7240cd5fbc5888d6edb73c9edcc45c97d
Avigdor Susana <avigdor@Avigdors-MacBook-Air-2.local>
2019-07-31 14:12:01 -0400

    hello 1.0.10

--- src/ios/kord.audio.swift
+++ src/ios/kord.audio.swift
@@ -1,10 +1,10 @@
 import Foundation
 
 @objc(HelloPlugin) class HelloPlugin : CDVPlugin {
-  func sayHello(_ command: CDVInvokedUrlCommand) {
-    let message = "Hello !";
+	func sayHello(_ command: CDVInvokedUrlCommand) {
+		let message = "Hello !";
 
-    let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: message);
-    commandDelegate.send(pluginResult, callbackId:command.callbackId);
-  }
+		let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: message);
+		commandDelegate.send(pluginResult, callbackId:command.callbackId);
+	}
 }
