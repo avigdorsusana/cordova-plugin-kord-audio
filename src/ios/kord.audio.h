de1a0aec91ef75c8fdebe3234c48083acf4663de
Avigdor Susana <avigdor@Avigdors-MacBook-Air-2.local>
2019-08-01 01:36:48 -0400

    hello 1.0.21

--- src/ios/kord.audio.h
+++ src/ios/kord.audio.h
@@ -1,7 +1,17 @@
 #import <Cordova/CDV.h>
+#import <AVFoundation/AVFoundation.h>
+#import "kord.audio.item.h"
 
-@interface pKORDaudio : CDVPlugin
+@interface DeviceAudioService : CDVPlugin {
+    NSMutableDictionary *registeredAudioElements;
+}
 
-- (void) greet:(CDVInvokedUrlCommand*)command;
+- (void)pluginInitialize;
+- (void)addAudioWithIdAndUrl:(CDVInvokedUrlCommand *)command;
+- (void)playAudioWithId:(CDVInvokedUrlCommand *)command;
+- (void)pauseAudioWithId:(CDVInvokedUrlCommand *)command;
+- (void)removeAllAudio:(CDVInvokedUrlCommand *)command;
+- (void)audioWithIdIsPaused:(CDVInvokedUrlCommand *)command;
+- (void)audioWithIdShouldResumePlayback:(CDVInvokedUrlCommand *)command;
 
 @end
--- www/kord.audio.js
+++ www/kord.audio.js
@@ -1,7 +1,31 @@
-/*global cordova, module*/
+var argscheck = require('cordova/argscheck'),
+    exec = require('cordova/exec');
 
-module.exports = {
-    greet: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Hello", "greet", [name]);
+var mplExport = {
+    addAudioWithIdAndUrl: function ( anId, aUrl, aSuccessCallback, aFailureCallback) {
+        return cordova.exec(aSuccessCallback, aFailureCallback, "DeviceAudioService", "addAudioWithIdAndUrl", [anId, aUrl]);
+    },    
+        
+    playAudioWithId: function (anId, aSuccessCallback, aFailureCallback) {
+        if(voices === undefined) voices = 1;
+        return cordova.exec(aSuccessCallback, aFailureCallback, "DeviceAudioService", "playAudioWithId", [anId]);
+    },
+        
+    pauseAudioWithId: function (anId, aSuccessCallback, aFailureCallback) {
+        return cordova.exec(aSuccessCallback, aFailureCallback, "DeviceAudioService", "pauseAudioWithId", [anId]);
+    },
+        
+    audioWithIdIsPaused: function (anId, aSuccessCallback, aFailureCallback) {
+        return cordova.exec(aSuccessCallback, aFailureCallback, "DeviceAudioService", "audioWithIdIsPaused", [anId]);
+    },
+        
+    audioWithIdShouldResumePlayback: function (anId, aSuccessCallback, aFailureCallback) {
+        return cordova.exec(aSuccessCallback, aFailureCallback, "DeviceAudioService", "audioWithIdShouldResumePlayback", [anId]);
+    },
+        
+    removeAllAudio: function (aSuccessCallback, aFailureCallback) {
+        return cordova.exec(aSuccessCallback, aFailureCallback, "DeviceAudioService", "removeAllAudio", []);
     }
 };
+
+module.exports = mplExport;
