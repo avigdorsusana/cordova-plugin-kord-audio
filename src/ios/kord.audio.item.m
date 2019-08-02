eba8647fa98bf09772bc33652cfbc0f5801ae845
Avigdor Susana <avigdor@Avigdors-MacBook-Air-2.local>
2019-08-02 01:01:29 -0400

    adding volume 7

--- src/ios/kord.audio.item.m
+++ src/ios/kord.audio.item.m
@@ -34,7 +34,7 @@ NSString* ERROR_AVPLAYER_FAILED = @"Error: AVPlayer instance failed";
 	//theVol = [[command.arguments objectAtIndex:1] floatValue];
 	//theVol = theVol / 100.0;
 	//if (theVol >= 0.0 || theVol <= 1.0) {
-		[[thePlayer] setVolume:0.0];
+		[thePlayer setVolume:0.0 atTime:kCMTimeZero];
 	//}
 	/*if ([thePlayer currentTime].value > 0) {
 		self.isPaused = YES;
